49c70bff89eafc56a26aa5dcf7032c3712da3ca3
易 蓦年 <smn9008@outlook.com>
2022-08-12 19:56:09 +0800

    v0.2.03

---
+++ 10Dictionary.py
@@ -0,0 +1,13 @@
+﻿#
+# How to build dictionary
+#
+dic1 = {'Mongoose':100,'Li hua':97,'Jim':69}        # 第一种
+dic2 = dict(name='Jack',score=76)                   # 第二种
+print('dic1结果1：',dic1)
+print('dic2结果1：',dic2)
+
+# 查找字典内元素
+print(dic1['Mongoose'])         # 不存在则报错
+print(dic1.get('Jim'))          # 不存在则返回none，推荐此种方法
+print(dic1.get('Michel',97))    # 不存在则返回设置默认值97
+
---
+++ 9List_2.py
@@ -0,0 +1,20 @@
+﻿#
+# 列表的排序操作
+#
+lst1=[9,3,2,6,1,7,8,4,5,3]
+lst1.sort()             # 按升序排列列表元素
+print('lst1的结果1为：',lst1)
+lst1.sort(reverse=True) # 按降序排列列表元素
+print('lst1的结果2为：',lst1)
+lst2=sorted(lst1)       # 按升序排列的列表另存为新列表
+print('lst2的结果1为：',lst2)
+lst2=sorted(lst1,reverse=True)  # 按降序排列的列表另存为新列表
+print('lst2的结果2为：',lst2)
+print('<-------------------------------------------->')
+
+
+# 列表生成
+lst3=[i*i for i in range(10)]       # i*i位置表示元素表达式，i表示自定义变量
+print('lst3的结果1为：',lst3)
+lst4=[i*2 for i in range(1:6)]      # 表示2，4，6，8，10
+print('lst4的结果1为：',lst4)

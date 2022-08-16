59616ca8fb893f5d999f4d2297013fbfa99b31c9
易 蓦年 <smn9008@outlook.com>
2022-08-16 21:36:43 +0800

    v0.2.07

--- 12CharacterString.py
+++ 12CharacterString.py
@@ -22,8 +22,112 @@ chr4=''.join(['ab','c'])
 print(chr3,id(chr3))
 print(chr4,id(chr4))
 print('chr3地址与chr4相同吗？',chr3 is chr4)
-
+print('<------------------------------------------->')
 
 #
 # 字符串查找
 # 
+chr5='Galaxy Tab'
+print(chr5.index('a'))      # 第一次出现的位置，1
+print(chr5.find('a'))       # 第一次出现的位置，1
+print(chr5.rindex('a'))     # 最后一次出现的位置，8
+print(chr5.rfind('a'))      # 最后一次出现的位置，8
+
+print(chr5.find('k'))       # 使用find()或rfind()函数查找不存在的字符，不报错，显示为
+                            # -1
+print('<-------------------------------------------->')
+
+
+#
+# 字符串大小写转换
+#
+print(chr5.upper())         # 所有字母转为大写
+# 内存地址已发生改变
+print(chr5.lower())         # 所有字母转为小写
+print(chr5.swapcase())      # 大写转为小写，小写转为大写
+print(chr5.capitalize())    # 首字母大写
+chr6='hello,world'
+print(chr6.title())         # 所有单词（忽略逗号）首字母大写，其余字母小写
+
+
+#
+# 字符串内容对齐操作
+#
+print(chr5.center(20,'-'))      # 该函数用于居中对齐，参数1指定宽度，若宽度小于字符串宽度，则
+                                # 直接居中，否则由参数2指定的填充符左右共计填充个 参数1-字符串
+                                # 宽度 个填充符。
+
+print(chr5.ljust(20,'-'))       # 该函数用于左对齐，参数1用于指定宽度，参数2指定填充符，若参数1
+                                # 大于参数2，则将 参数1-字符串宽度 个填充符加在字符串右侧，否则
+                                # 直接左对齐。
+
+print(chr5.rjust(20,'-'))       # 该函数用于右对齐，参数1用于指定宽度，参数2指定填充符，若参数1
+                                # 大于参数2，则将 参数1-字符串宽度 个填充符加在字符串左侧，否则
+                                # 直接右对齐。
+
+print(chr5.zfill(20))           # 右对齐，左边用0填充，只有一个参数，用于指定宽度，如果参数小于
+                                # 字符串宽度，则直接右对齐
+print('-8901'.zfill(10))        # 0添加到负号后面，且只添加3个
+print('<------------------------------------------------>')
+
+
+#
+# 字符串的分割
+#
+chr7='Hello world Python'
+print(chr7.split())             # 不指定参数，默认按空格将字符串分为多个元素，并以列表呈现
+chr8='Hello#world#Python'
+print(chr8.split(sep='#'))      # 使用'sep='等式指定分隔符为','，结果与上式相同
+print(chr8.split(sep='#',maxsplit=1))   # 使用'maxsplit='等式指定分隔个数，其余部分则作为一个
+                                        # 整体存在列表中
+
+print(chr8.rsplit(sep='#',maxsplit=1))  # 与split()的区别只在使用了等式'maxsplit='时从右计数
+
+
+#
+# 字符串的判断
+#
+print('init'.isidentifier())            # 判断字符串是否为合法的标识符
+print('\t'.isspace())                   # 判断字符串是否为空白字符
+print('abc'.isalpha())                  # 判断字符串是否全为字母
+print('b000010'.isdecimal())            # 判断字符串是否全为十进制数字
+print('123'.isnumeric())                # 判断字符串是否全为数字
+print('123 b5'.isalpha())               # 判断字符串是否全为数字和字母
+
+
+# 字符串的替换
+print(chr7.replace('Python','Java'))    # 替换选定的字符串为新字符串
+chr9='Do what you cant to Do'
+print(chr9.replace('ou','test'))
+
+# 字符串合并
+list1=['hello','python','today']
+chr10='#'.join(list1)                   # 将列表内元素连接为一个字符串，连接分隔符为‘#’，默认为
+                                        # 直接连接
+print(chr10,type(chr10))
+
+tup1={'hello','python','today'}
+chr11='#'.join(tup1)                    # 将元组内元素连接为一个字符串
+print(chr11,type(chr11))
+
+
+#
+# 字符串的比较
+#
+chr12='apple'
+chr13='banana'
+print('chr12>chr13? ',chr12>chr13)      # 由于两字符串的第一位就是不同的，所以比较这一位置的字符
+                                        # 在unicode编码中的数值，a<b，故而chr12<chr13
+
+list1=['>','>=','<','<=','==','!=']     # 有这些运算符
+
+
+#
+# 字符串的切片操作
+#
+chr7='Hello world Python'
+option1=chr7[:5]
+option2=chr7[6:]
+
+print(option1)
+print(option2)
--- 3EncodeANDIdentifier.py
+++ 3EncodeANDIdentifier.py
@@ -1,5 +1,12 @@
 ﻿# Import keyword list
 import keyword
-print(keyword.kwlist)
+print(keyword.kwlist)       # 显示python中的所有保留字
+print('<--------------------------------------------->\n')
 
 # Watch out the file encode format, only unicode can identify zh-CN 
+
+print(ord('a'))         # 可以显示字符a的Unicode编码数值
+print(chr(97))          # 可以显示Unicode编码序列97对应的字符
+
+print(ord('莫'))
+print(chr(33707))

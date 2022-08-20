98d69fcb6a31d33191c37fc43d9902086710bb24
易 蓦年 <smn9008@outlook.com>
2022-08-20 17:09:38 +0800

    v0.2.10

--- 15Class.py
+++ 15Class.py
@@ -1,1 +1,75 @@
-﻿
+﻿#
+# 类的创建
+#
+class Student:                      # class 之后为自定义类名
+    native_space='HAK'               # 类属性
+    
+    def __init__(self,name,age):    # 初始化方法
+        self.name=name              # 实例属性
+        self.age=age
+
+    def eat(self):                  # 实例方法，括号内 self 是默认参数
+        print('They are eating.')
+
+    @staticmethod                   # 引用 staticmethod 以创建静态方法，无默认参数
+    def method():
+        print('default output.')
+
+    @classmethod                    # 创建类方法，括号内 cls 是默认参数
+    def cm(cls):
+        print('default classmethod.')
+
+
+#
+# 对象的创建
+#
+stu1=Student('LiHua',21)            # 创建一个Student类的对象
+stu1.eat()                          # 调用实例方法，对象名.方法名()
+print(stu1.name)                    # 显示属性
+print(stu1.age)
+
+Student.eat(stu1)                   # 调用实例方法，类名.方法名(类的对象)-->实际就是方法
+                                    # 定义处的 self
+print('<------------------------------------------------>')
+
+
+#
+# 类属性
+#
+print(Student.native_space)
+stu2=Student('Jack',20)             # 创建该类的对象，Jack传入初始化方法name形参，并赋值给
+stu3=Student('Mike',21)             # self.name，20传入形参age，并赋值self.age
+
+print(stu1.native_space)            # 显示类属性
+print(stu2.native_space)
+
+Student.native_space='CKG'          # 更改类属性
+print(stu1.native_space)
+print(stu2.native_space)
+print('<--------------------------------------->')
+
+
+#
+# 类方法
+#
+Student.cm()                        # 调用类方法
+
+
+#
+# 静态方法
+#
+Student.method()                    # 调用静态方法
+
+
+#
+# 动态绑定属性和方法
+#
+stu2.gender='female'                # 为对象stu2添加实例属性
+print(stu2.name,stu2.age,stu2.gender)
+stu1.eat()
+stu2.eat()
+
+def exp():
+    print('Default funtion1.')
+stu1.func1=exp                      # 为stu1创建一个新的方法，绑定函数exp()
+stu1.func1()                        # 调用该方法
---
+++ 16ObjectClass.py
@@ -0,0 +1,79 @@
+﻿#
+# 封装
+#
+class Car:
+    def __init__(self,brand):
+        self.brand=brand
+    def start(self):
+        print('The Car has been started...')
+car=Car('Audi')
+car.start()
+print(car.brand)
+print('<----------------------------------------------------------->')
+
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.__age=age              # 加两个下划线则在类外部不可被直接引用
+    def exp1(self):
+        print(self.name,self.__age)
+stu1=Student('Michael',22)
+stu1.exp1()
+print(stu1.name)
+# 如果想要引用 age 属性
+print(dir(stu1),'\n')           # 首先获取stu1对象所包含的所有属性
+print(stu1._Student__age)       # 找到对应的名字，引用即可
+print('<------------------------------------------------->')
+
+
+#
+# 继承
+#
+class Person(object):                           # 父级
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def info(self):
+        print(self.name,self.age)
+
+class Student(Person):                          # 子集1
+    def __init__(self,name,age,stu_num):
+        super().__init__(name,age)
+        self.stu_num=stu_num 
+    def info(self):
+        super().info()                          # 重写父类中的 info() 方法，通过super().func()
+                                                # 即可重写方法
+        print(self.stu_num)
+
+class Teacher(Person):                          # 子集2
+    def __init__(self,name,age,teachofyear):
+        super().__init__(name,age)
+        self.teachofyear=teachofyear
+    def info(self):
+        super().info()                          # 重用
+        print(self.teachofyear)
+
+stu1=Student('Micheal',22,1659473)
+teach1=Teacher('Miss Tab',34,10)
+stu1.info()                                     # 子集1继承
+teach1.info()                                   # 子集2继承
+print('<------------------------------------------------------->')
+
+
+#
+# object类
+#
+class Defau:
+    def __init__(self,name,age):
+        self.name=name 
+        self.age=age 
+    def __str__(self):          # __str__()方法用于返回对象描述，如果重写则用于查看对象信息
+        return 'My name is {0}, and I am {1} years old.'.format(self.name,self.age)
+defau1=Defau('Sam',23)
+print(dir(defau1),'\n')
+print(defau1)
+
+
+#
+# 多态
+#

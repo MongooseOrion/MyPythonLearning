8eef8ba894737061a61a19f6766f8259c041798d
易 蓦年 <smn9008@outlook.com>
2022-08-24 15:47:23 +0800

    v0.3.01(22.8.24.01)

---
+++ MyPythonLearning2/01Module_1.py
@@ -0,0 +1,11 @@
+
+import math                 # 导入数学常数模块
+print(id(math))
+print(type(math))
+print(math)
+print(math.e)
+print(dir(math))            # 查看math的所有属性
+print(math.pow(2,3))        # 指的是2^3
+print(math.ceil(9.001))     # 向上取整
+print(math.floor(9.999))    # 向下取整
+

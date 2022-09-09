6a50ee4cc92603ab3db69b007e729a8d6992c51e
易 蓦年 <smn9008@outlook.com>
2022-09-09 15:33:04 +0800

    v0.3.10

--- MyPythonLearning2/Module_2.py
+++ MyPythonLearning2/Module_2.py
@@ -1,7 +1,7 @@
 ﻿#
 # 导入 Python 内置模块
 #
-'''
+
 import sys
 print(dir(sys))                                 # 查看 sys 内的所有函数
 print('True的字节数：',sys.getsizeof(True))      # 获取指定字符的数据占用量
@@ -14,7 +14,7 @@ print(time.localtime(time.time()))          # 转换为当前时间
 
 import urllib.request as a
 print(a.urlopen('https://cn.bing.com').read())      # 读取该网页所有内容并返回
-'''
+
 
 #
 # 导入 pyecharts 绘制图表

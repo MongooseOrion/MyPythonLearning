74c1a82da4204e9a73585cc99900297cb37f7a66
Yi Monian <YiMonian@outlook.com>
2022-10-27 18:00:00 +0800

    v1.0.03

--- MyPythonLearning2/Module_4.py
+++ MyPythonLearning2/Module_4.py
@@ -31,5 +31,22 @@ with open('alipay.ico','rb') as src_file:
         target_file.write(src_file.read())
 
 
+#
+# 压缩解压缩文件
+#
+import os
+import zipfile
 
+dir_list = os.listdir()                 # 将当前目录的所有文件存入目录
+# print(dir_list)
 
+# 使用 w 是压缩模式，r 是读取模式
+with zipfile.ZipFile('filezip.zip','w') as zipfiles:
+    for file in dir_list:
+        if file.endswith('.html'):      # 将所有的以.html 结尾的内容压缩
+            zipfiles.write(file)
+
+with zipfile.ZipFile('filezip.zip','r') as zipfiles:
+    print(zipfiles.namelist())          # 显示该压缩包内的所有文件名称
+    zipfiles.extract('全国疫情地图.html','./')    # 解压单个文件到本文件夹
+    zipfiles.extractall('zipfiles/')            # 解压所有内容到项目文件夹/zipfiles/内
